28847b2d7aa752cd51366aed072a3082e02fc4d7
Monica Rangel <joanna.monica@yahoo.com>
2020-05-26 18:08:48 -0700

    adding password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+function getRandomLower(){}
+    return string.fromCharcode(97)
   // you can create a function named generatePassword that creates the password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,6 +12,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+console.log(math.random()*26;
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
