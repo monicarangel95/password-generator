007a44247fe8c37418b99b9c24484f33222f338a
Monica Rangel <joanna.monica@yahoo.com>
2020-05-26 18:19:00 -0700

    adding pass length

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-function getRandomLower(){}
-    return string.fromCharcode(97)
+
+
   // you can create a function named generatePassword that creates the password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,7 +13,35 @@ function getRandomLower(){}
 
 }
 
-console.log(math.random()*26;
+function generatePassword() {
+
+// array 
+var chosen = [];
+
+// asking user password criteria
+let upper = confirm("Insert uppercase letters?");
+let lower = confirm("Insert lowercase letters?");
+let special = confirm("Insert special characters?");
+var number = confirm("Insert numbers?");
+
+// validating input
+while((upper === false) && (lower === false) && (special === false) && (number === false)) {
+alert ("Choose a specification:");
+let upper = confirm("Insert uppercase letters?");
+let lower = confirm("Insert lowercase letters?");
+let special = confirm("Insert special characters?");
+var number = confirm("Insert numbers?");
+}
+
+// password length 
+var passLength = prompt ("How long would you like your password? Please choose between 8-128.");
+while ((passLength < 8) || (passLength > 128)){
+  alert("Please choose between 8-128.");
+  passLength = prompt("How long would you like your password?");
+} 
+
+
+}
 
 
 // Add event listener to generate button
