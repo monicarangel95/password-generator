2d76f40e2e9e3ac3723a92fd3790664fd8c2c984
Monica Rangel <joanna.monica@yahoo.com>
2020-05-26 18:52:15 -0700

    adding final result

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,51 +19,68 @@ function generatePassword() {
 var chosen = [];
 
 // asking user password criteria
-let upper = confirm("Insert uppercase letters?");
-let lower = confirm("Insert lowercase letters?");
-let special = confirm("Insert special characters?");
+var upper = confirm("Insert uppercase letters?");
+var lower = confirm("Insert lowercase letters?");
+var special = confirm("Insert special characters?");
 var number = confirm("Insert numbers?");
 
 // validating input
 while((upper === false) && (lower === false) && (special === false) && (number === false)) {
 alert ("Choose a specification:");
-let upper = confirm("Insert uppercase letters?");
-let lower = confirm("Insert lowercase letters?");
-let special = confirm("Insert special characters?");
+var upper = confirm("Insert uppercase letters?");
+var lower = confirm("Insert lowercase letters?");
+var special = confirm("Insert special characters?");
 var number = confirm("Insert numbers?");
 }
 
 // password length 
-var passLength = prompt ("How long would you like your password? Please choose between 8-128.");
-while ((passLength < 8) || (passLength > 128)){
+var userLength = prompt ("How long would you like your password? Please choose between 8-128.");
+while ((useerLength < 8) || (passLength > 128)){
   alert("Please choose between 8-128.");
-  passLength = prompt("How long would you like your password?");
+  userLength = prompt("How long would you like your password?");
 } 
 // array for pass criteria
-let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let specialChar = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
 var num = ["0","1","2","3","4","5","6","7","8","9"];
 
 // uppercase inserted and chosen
 if (upper) {
   for(var i = 0; i < upperCase. length; i++ ){
-    inserted.chosen(upperCase[i]);
+    chosen.push(upperCase[i]);
 
   }
 }
 // lowercase inserted and chosen
 if(lower){
   for(var j=0; j < lowerCase.length; j++){
-    inserted.chosen(lowerCase[j]);
+    chosen.push(lowerCase[j]);
   }
-
-
-
-
-
+}
+// special inserted and chosen
+if(special){
+  for(var k=0; k < specialChar.length; k++){
+    chosen.push(specialChar[k]);
+  }
 }
 
+// numbers inserted and chosen
+if(number){
+  for(var l=0; l < num.length; l++){
+    chosen.push(num[l]);
+  }
+}
+// final result stores all password critera 
+var finalResult = [];
+for(m=0; m < userLength; m++){
+  finalResult.push(chosen[Math.floor(Math.random()*chosen.length)]);
+}
+
+// return finalResult with randomized chars as long as userLength joining array into strings
+return(finalResult.join(""));
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
